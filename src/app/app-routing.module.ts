c0a6309b5338d6859841aa03ae49547a6dceda93
Caleb Piekstra <cpiekstra@newrelic.com>
2019-10-03 19:14:18 -0700

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlannerComponent } from './planner/planner.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'planner', component: PlannerComponent },
+  { path: 'welcome', component: AppComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  { path: '**', component: AppComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
